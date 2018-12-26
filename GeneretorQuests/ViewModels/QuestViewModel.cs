59abc050be2b6f7caadd10b08557c0a05cc1c183
Татьяна Солодковская <solodkovskaya201098@outlook.com>
2018-12-26 23:04:37 +0300

    update design

--- GeneretorQuests/ViewModels/QuestViewModel.cs
+++ GeneretorQuests/ViewModels/QuestViewModel.cs
@@ -18,17 +18,31 @@ namespace GeneretorQuests.ViewModels
         public DBRepos db;
         private Quest selectedQuest;
         private User User;
-       // private RiddleModel selectedRiddle;
-          
+        // private RiddleModel selectedRiddle;
+        bool isChecked;
+
+        public bool IsChecked
+        {
+            get { return isChecked; }
+            set
+            {
+                isChecked = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("IsChecked"));
+                if (!isChecked) Quests = rep.GetAllQuest();
+                else Quests = new ObservableCollection<Quest> (rep.GetAllQuest().Where(i => i.Id_Autor_FK == User.Id_user));
+            }
+        }
         private readonly IDialogManager _dialogManager;
         public QuestViewModel(IDialogManager dialogManager, User user)
         {
             _dialogManager = dialogManager;
+           
             this.Quests = new ObservableCollection<Quest>();
             db = new DBRepos();
             rep = new DbDataOperation(db);
             User = user;
             Quests = rep.GetAllQuest();
+            isChecked = false;
         }
         private RelayCommand openMain;
         public RelayCommand OpenMain
